6909f38102d459ce907cf405454bfa39dce01f00
Mikael Dalholm <mikael.dalholm@exaktasoftware.se>
2020-02-01 13:14:01 +0100

    Disable host check on dev server

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.webpackConfig({
+    devServer:{
+        disableHostCheck: true
+    },
+});
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
