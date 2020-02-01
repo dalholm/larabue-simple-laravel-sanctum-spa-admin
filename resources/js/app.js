3ed8d9d26ac44d96091ffacb866b3880ba377254
Mikael Dalholm <mikael.dalholm@exaktasoftware.se>
2020-02-01 13:29:40 +0100

    setup due and buefy with base theme

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-//
+import Vue from 'vue';
+import Buefy from 'buefy'
+
+import App from './App.vue';
+
+Vue.use(Buefy);
+
+new Vue({
+    ...App
+}).$mount('#app');
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Larabue</title>
+
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
         <div id="app"></div>
+
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
