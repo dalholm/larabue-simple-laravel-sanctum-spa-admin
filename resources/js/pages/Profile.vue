51037d6516f14e67c2e6cf3f802d3a965da55e48
Mikael Dalholm <mikael.dalholm@exaktasoftware.se>
2020-02-09 17:37:10 +0100

    design fixes on profile

--- resources/js/pages/Profile.vue
+++ resources/js/pages/Profile.vue
@@ -1,86 +1,87 @@
 <template>
     <div>
-        <div class="column is-half">
-            <h2 class="title is-2">Profile</h2>
-            <div class="subtitle">Update your profile</div>
+        <section>
+            <h2 class="title is-2">{{user.name}}</h2>
+            <div class="subtitle is-5">Profile and settings</div>
 
             <p>Registred: {{user.created_at}}</p>
             <p>Last updated: {{user.updated_at}}</p>
             <div class="divider"></div>
-            <form method="#" @submit.prevent="updateProfile">
-
-                <b-field label="Name"
-                         :type="{'is-danger': hasError('name')}"
-                         :message="errorLabel('name')">
-                    <b-input
-                        placeholder="Name"
-                        icon="account"
-                        v-model="form.name"
-                    ></b-input>
-                </b-field>
-
-                <b-field label="Email"
-                         :type="{'is-danger': hasError('email')}"
-                         :message="errorLabel('email')">
-                    <b-input
-                        placeholder="Email"
-                        icon="email"
-                        v-model="form.email"
-                    ></b-input>
-                </b-field>
-
-                <b-field>
-                    <b-button
-                        type="is-primary"
-                        native-type="submit"
-                        icon-left="content-save">
-                        Update
-                    </b-button>
-                </b-field>
-            </form>
-
-            <div class="divider"></div>
-
-            <section>
-                <h4 class="title is-4">Update password</h4>
-                <form @submit.prevent="updatePassword">
-
-                    <b-field label="Password"
-                             :type="{'is-danger': hasError('password')}"
-                             :message="errorLabel('password')">
+        </section>
+        <div class="columns">
+            <div class="column">
+                <h4 class="title is-4">Update profile</h4>
+                <form method="#" @submit.prevent="updateProfile">
+                    <b-field label="Name"
+                             :type="{'is-danger': hasError('name')}"
+                             :message="errorLabel('name')">
                         <b-input
-                            type="password"
-                            placeholder="Password"
-                            icon="lock"
-                            password-reveal
-                            v-model="form.password"
+                            placeholder="Name"
+                            icon="account"
+                            v-model="form.name"
                         ></b-input>
                     </b-field>
 
-                    <b-field label="Repeat password"
-                             :type="{'is-danger': hasError('password_confirmation')}"
-                             :message="errorLabel('password_confirmation')">
+                    <b-field label="Email"
+                             :type="{'is-danger': hasError('email')}"
+                             :message="errorLabel('email')">
                         <b-input
-                            type="password"
-                            placeholder="Password"
-                            icon="lock"
-                            password-reveal
-                            v-model="form.password_confirmation"
+                            placeholder="Email"
+                            icon="email"
+                            v-model="form.email"
                         ></b-input>
                     </b-field>
+
                     <b-field>
                         <b-button
                             type="is-primary"
                             native-type="submit"
                             icon-left="content-save">
-                            Update password
+                            Update
                         </b-button>
                     </b-field>
-
                 </form>
-            </section>
-        </div>
+            </div>
+            <div class="column">
+                <section>
+                    <h4 class="title is-4">Update password</h4>
+                    <form @submit.prevent="updatePassword">
+                        <b-field label="Password"
+                                 :type="{'is-danger': hasError('password')}"
+                                 :message="errorLabel('password')">
+                            <b-input
+                                type="password"
+                                placeholder="Password"
+                                icon="lock"
+                                password-reveal
+                                v-model="form.password"
+                            ></b-input>
+                        </b-field>
 
+                        <b-field label="Repeat password"
+                                 :type="{'is-danger': hasError('password_confirmation')}"
+                                 :message="errorLabel('password_confirmation')">
+                            <b-input
+                                type="password"
+                                placeholder="Password"
+                                icon="lock"
+                                password-reveal
+                                v-model="form.password_confirmation"
+                            ></b-input>
+                        </b-field>
+                        <b-field>
+                            <b-button
+                                type="is-primary"
+                                native-type="submit"
+                                icon-left="content-save">
+                                Update password
+                            </b-button>
+                        </b-field>
+
+                    </form>
+                </section>
+            </div>
+        </div>
     </div>
 </template>
 
