9ba74bfd3e948abbd4798a0f7d446ae9d4098f18
Mikael Dalholm <mikael.dalholm@exaktasoftware.se>
2020-08-17 16:32:32 +0200

    Initial setup

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        Spatie\Permission\PermissionServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
